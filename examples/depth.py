56a030a31b9ee57fab0d467f63ae44af40dd16d9
Xavier Mendez <jmendeth@gmail.com>
2012-03-08 20:04:07 +0100

    Update depth.py example to use the new feature

--- examples/depth.py
+++ examples/depth.py
@@ -30,13 +30,12 @@ while True:
     # Update to next frame
     nRetVal = ctx.wait_one_update_all(depth)
 
-    depthMap = depth.get_tuple_depth_map()
-    res = depth.metadata.res
+    dmap = depth.map
 
     # Calculate index of middle pixel
-    middleIdx = res[0] * (res[1]/2) # start of the middle line...
-    middleIdx += (res[0]/2)         # plus the middle of that line
+    middleIdx = dmap.width * (dmap.height/2) # start of the middle line...
+    middleIdx += (dmap.width/2)              # plus the middle of that line
 
-    print "The middle pixel is %d millimeters away." % depthMap[middleIdx]
+    print "The middle pixel is %d millimeters away." % dmap[middleIdx]
 
 
